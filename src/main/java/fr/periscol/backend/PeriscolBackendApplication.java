fa62a34c3648001b2eb2aa88baedbb8ec850572a
BenjaminNavet <benjamin.navet@live.fr>
2022-01-04 14:58:00 +0100

    initial

---
+++ src/main/java/fr/periscol/backend/PeriscolBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.periscol.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeriscolBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeriscolBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/periscol/backend/PeriscolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.periscol.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeriscolBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
